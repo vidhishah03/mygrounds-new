2f6bd1e48e77e2598699437099851c417a0c9760
twilight-warlock <devansh.ts@somaiya.edu>
2021-01-23 20:02:18 +0530

    Home Page Added

--- turfwebsite/turf/views.py
+++ turfwebsite/turf/views.py
@@ -1,3 +1,8 @@
+import os
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def home_view(request, *args, **kwargs):
+    return render(request, os.path.join("turf", "index.html"), {})
--- turfwebsite/turfwebsite/settings.py
+++ turfwebsite/turfwebsite/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'turfwebsite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, "templates")],
+        'DIRS': [os.path.join(BASE_DIR, "template")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
