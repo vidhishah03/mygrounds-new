588a198cb4dc237e46eddcd746eed743c7b31b59
bhavik <shadow7bm@gmail.com>
2021-01-24 19:13:52 +0530

    a

--- turfwebsite/turfwebsite/settings.py
+++ turfwebsite/turfwebsite/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'turf.apps.TurfConfig',
-    'accounts',
 ]
 
 MIDDLEWARE = [
--- turfwebsite/turfwebsite/urls.py
+++ turfwebsite/turfwebsite/urls.py
@@ -26,6 +26,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('turf.urls')),
-    path('accounts/', include('accounts.urls')),
-    path('accounts/', include('django.contrib.auth.urls')),
+    path('turf/', include('django.contrib.auth.urls')),
 ]
