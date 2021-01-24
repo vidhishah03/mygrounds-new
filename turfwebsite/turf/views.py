cf412bd1253653620afaab88cab3b6b2e42a0a39
bhavik <shadow7bm@gmail.com>
2021-01-24 19:32:05 +0530

    SignUp added

--- turfwebsite/turf/views.py
+++ turfwebsite/turf/views.py
@@ -1,8 +1,15 @@
 import os
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views import generic
 # Create your views here.
 
 
 def home_view(request, *args, **kwargs):
     return render(request, "turf/landingpage.html")
+
+class SignUpView(generic.CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'registration/signup.html'
