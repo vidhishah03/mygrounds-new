98ee036f3452cbf1a9d2bf28264356d6998bb53e
twilight-warlock <devansh.ts@somaiya.edu>
2021-01-26 21:19:37 +0530

    Made turf model alterations

--- turfwebsite/turf/forms.py
+++ turfwebsite/turf/forms.py
@@ -6,5 +6,5 @@ class TurfForm(forms.ModelForm):
 
     class Meta:
         model = Turf_List
-        fields = ['name', 'address', 'image', 'facilities', 'near_by_turf',
-                  'emergency', 'events',  'num_5v5', 'num_7v7',  'num_7v7_5v5',  'num_12v12']
+        fields = ['name', 'address', 'image', 'facilities',
+                  'emergency', 'events',  'num_5v5', 'price_per_hour_5v5', 'num_7v7', 'price_per_hour_7v7',  'num_7v7_5v5',  'num_12v12', 'price_per_hour_12v12']
--- turfwebsite/turf/models.py
+++ turfwebsite/turf/models.py
@@ -8,16 +8,22 @@ class Turf_List(models.Model):
     address = models.CharField(max_length=500, default='')
     image = models.ImageField(default='', upload_to="../media")
     facilities = models.CharField(max_length=500, default='')
-    near_by_turf = models.CharField(max_length=500, default='')
+    # near_by_turf = models.CharField(max_length=500, default='')
     emergency = models.CharField(max_length=500, default='')
     events = models.CharField(max_length=500, default='')
     num_5v5 = models.IntegerField(default=0)
+    price_per_hour_5v5 = models.IntegerField(default=500)
     num_7v7 = models.IntegerField(default=0)
+    price_per_hour_7v7 = models.IntegerField(default=700)
     num_7v7_5v5 = models.IntegerField(default=0)
     num_12v12 = models.IntegerField(default=0)
+    price_per_hour_12v12 = models.IntegerField(default=1200)
 
     def __str__(self):
         return self.name
+
+# Reservation Model
+
 
 # # creating a form
 # class DetailsForm(forms.Form):
