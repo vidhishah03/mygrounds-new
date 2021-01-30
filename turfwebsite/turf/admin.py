933fac6ca768dc47ab6807520f703d31666b336d
bhavik <shadow7bm@gmail.com>
2021-01-31 00:53:50 +0530

    models changes

--- turfwebsite/turf/admin.py
+++ turfwebsite/turf/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 
-from .models import Turf_List,User
+from .models import *
 
 admin.site.register(Turf_List)
+admin.site.register(Booking)
+admin.site.register(Contact)
+admin.site.register(feedback)
+
--- turfwebsite/turf/filters.py
+++ turfwebsite/turf/filters.py
@@ -6,4 +6,5 @@ class Turf_ListFilter(django_filters.FilterSet):
     class Meta:
         model = Turf_List
         fields = '__all__'
-        exclude = ['image','has_refreshments','events','turf_id']
+        exclude = ['image','weekdays_turf_opening_time','weekdays_turf_closing_time','events','turf_id',
+        'weekdends_turf_opening_time','weekends_turf_closing_time','price_per_hour_5v5_weekdays','price_per_hour_5v5_weekends']
