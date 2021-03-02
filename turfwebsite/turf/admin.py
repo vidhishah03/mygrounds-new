59f5caa2e9a91ef966fb4ca3fd70d4e55144ea94
vidhishah03 <58200995+vidhishah03@users.noreply.github.com>
2021-03-03 00:21:09 +0530

    Delete turfwebsite directory

--- turfwebsite/turf/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import *
-
-admin.site.register(Turf_List)
-admin.site.register(Booking)
-admin.site.register(Contact)
-admin.site.register(feedback)
-
--- turfwebsite/turf/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class TurfConfig(AppConfig):
-    name = 'turf'
