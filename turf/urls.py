35fa27e41d54bc63ab525114b69ff223a2a35ee4
vidhi.0311shah <vidhi11@somaiya.edu>
2021-03-03 01:25:37 +0530

    deploy

--- turf/urls.py
+++ turf/urls.py
@@ -6,7 +6,7 @@ from .views import SignUpView, myaccountview, passwordchangeview, editturfview
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('', views.home_view, name='home'),
+    path('/', views.home_view, name='home'),
     path('signup/', SignUpView.as_view(), name='signup'),
     path('turfdetails/', views.detailsform_view, name='turfdetails'),
     path('display/', views.show_turf, name="display"),
